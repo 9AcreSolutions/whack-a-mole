955760d36ad06157b8ee8d37b1abd745b8b620a1
dfe325 <daneder@gmail.com>
2022-06-06 16:15:53 -0400

    adds bootstrap, venv, db

--- whack-a-mole/__init__.py
+++ whack-a-mole/__init__.py
@@ -1,11 +1,13 @@
 import os
 
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, request, session, flash
 
 
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
+    app.SECRET_KEY = "my precious"
+
     app.config.from_mapping(
         SECRET_KEY='dev',
         DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
@@ -35,4 +37,27 @@ def create_app(test_config=None):
     if __name__ == '__main__':
         app.run()
 
+    # Route for handling the login page logic
+    @app.route('/login', methods=['GET', 'POST'])
+    def login():
+        error = None
+        if request.method == 'POST':
+            if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+                error = 'Invalid Credentials. Please try again.'
+            else:
+                session['logged_in'] = True
+                flash('You were just logged in!')
+                return redirect(url_for('home'))
+        return render_template('login.html', error=error)
+
+    @app.route('/logout')
+    def logout():
+        session.pop('logged_in', None)
+        flash('You were just logged out !')
+        return redirect(url_for('hello'))
+
     return app
+
+# # start the server with the 'run()' method
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ whack-a-mole/app.py
@@ -0,0 +1,28 @@
+# from flask import Flask, render_template, redirect, url_for, request
+#
+# app = Flask(__name__)
+#
+# @app.route('/')
+# def home():
+#     return render_template('index-START.html')
+# if __name__ == '__main__':
+#     app.run()
+#
+#
+# @app.route("/hello")
+# def home_view():
+#         return "<h1>Yo b*tch!</h1>"
+#
+# @app.route('/login', methods=['GET', 'POST'])
+# def login():
+#     error = None
+#     if request.method == 'POST':
+#         if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+#             error = 'Invalid Credentials. Please try again.'
+#         else:
+#             return redirect(url_for('home'))
+#     return render_template('login.html', error=error)
+#
+# # start the server with the 'run()' method
+# if __name__ == '__main__':
+#     app.run(debug=True)
